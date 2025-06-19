91fb47c4043d682bf5eab92df8914ba25204457d
meera0305 <chavanmeera66@gmail.com>
2025-06-19 15:47:18 +0530

    Create schema.sql

---
+++ schema.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE IF NOT EXISTS raksha_security;
+USE raksha_security;
+
+CREATE TABLE users (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(100),
+  phone VARCHAR(15),
+  email VARCHAR(100),
+  password VARCHAR(100)
+);
+
+CREATE TABLE alerts (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  user_id INT,
+  location VARCHAR(255),
+  time DATETIME,
+  FOREIGN KEY (user_id) REFERENCES users(id)
+);
