ec196a9cc0310b8407b9d4e140d7b569ec72af73
Meera Santram Chavan <chavanmeera66@gmail.com>
2025-06-27 12:32:25 +0530

    Create alert_model.py

---
+++ alert_model.py
@@ -0,0 +1,8 @@
+import sqlite3
+
+def save_alert(name, location):
+    con = sqlite3.connect("alerts.db")
+    cur = con.cursor()
+    cur.execute("INSERT INTO alerts (name, location) VALUES (?, ?)", (name, location))
+    con.commit()
+    con.close()
